7d05c99faa1f538c63b5547f86238727b88a3ad5
mostlyclicks <carlos@mostlyclicks.com>
2013-09-03 16:35:25 -0500

    config assets initialize on precomp

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Wieser6
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #http://refinerycms.com/guides/heroku
+    config.assets.initialize_on_precompile = true
   end
 end
