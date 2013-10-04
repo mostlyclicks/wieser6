7786de25c21e15ee89081e81e6cf8f3dcd71f6f7
mostlyclicks <carlos@mostlyclicks.com>
2013-10-03 21:36:58 -0500

    carousel

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,9 +1,16 @@
 Refinery::PagesController.class_eval do
+	#include Refinery::Project::ControllerHelper
 	before_filter :find_archived_projects
+	before_filter :list_of_projects
 
 	protected
 
 		def find_archived_projects
 			@archived_projects = "Hello"
+		end
+
+		def list_of_projects
+			@projects_for_home = Refinery::Projects::Project.where project_status: "1"
+			@project_for_home = Refinery::Projects::Project.first
 		end
 end
